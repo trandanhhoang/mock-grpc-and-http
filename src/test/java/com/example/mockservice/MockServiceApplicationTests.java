ec6e97b123651a59da6fdb39b53e17ae67c2dd7e
hoangtd4 <hoangtd4@vng.com.vn>
2024-09-26 14:50:35 +0700

    initial commit

---
+++ src/test/java/com/example/mockservice/MockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
